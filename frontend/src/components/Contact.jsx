2983134842d0f21161b2ad69d21baad67603a8f1
Dikshant Shinde <dikshant2426D@gmail.com>
2026-05-11 18:03:47 +0530

    Updated favicon

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -25,7 +25,7 @@ const SOCIAL_LINKS = [
   },
   {
     label: 'LinkedIn',
-    href: '#',
+    href: 'https://linkedin.com/in/dikshant-shinde',
     icon: (
       <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
         <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6zM2 9h4v12H2z" />
