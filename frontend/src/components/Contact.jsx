82eec6dbb73e1518e4a8774667af71899f3e7d5c
Dikshant Shinde <dikshant2426D@gmail.com>
2026-05-12 12:14:14 +0530

    LinkedIn contact added

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -25,7 +25,7 @@ const SOCIAL_LINKS = [
   },
   {
     label: 'LinkedIn',
-    href: 'https://linkedin.com/in/dikshant-shinde',
+    href: ' https://in.linkedin.com/in/dikshant-shinde-213713408',
     icon: (
       <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
         <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6zM2 9h4v12H2z" />
@@ -201,6 +201,17 @@ export default function Contact() {
                         }}
                       >
                         github.com/UMBRELLACORP007
+                      </div>
+                    )}
+                    {link.label === 'LinkedIn' && (
+                      <div
+                        style={{
+                          fontFamily: 'DM Mono, monospace',
+                          fontSize: '11px',
+                          color: '#4a5a6e',
+                        }}
+                      >
+                        in.linkedin.com/in/dikshant-shinde-213713408
                       </div>
                     )}
                   </div>
